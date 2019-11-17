6be4a96ab904a22b44380992c328713fb9d5742d
Yegor Chechurin <yegor.chechurin@gmail.com>
2019-11-17 15:52:29 +0300

    created fixtures for User entity

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+	private $passwordEncoder;
+
+	public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i=0; $i < 5; $i++) { 
+        	$number = $i+1;
+    		$user = new User();
+    		$user->setUsername('user'.$number);
+    		$user->setEmail('user'.$number);
+    		$user->setPassword(
+    			$this->passwordEncoder->encodePassword(
+    				$user,
+	            	'user'.$number
+	        	)
+    		);
+        	$manager->persist($user);
+    	}
+
+        $manager->flush();
+    }
+}
